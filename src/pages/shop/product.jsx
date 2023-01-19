4979d0d56ddc9662ac7659814578f9a1441f604a
Amin-chak99 <76822547+Amin-chak99@users.noreply.github.com>
2023-01-20 00:23:25 +0100

    commit

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,5 +1,9 @@
 import React, { useContext } from "react";
 import { ShopContext } from "../../context/shop-context";
+import Card from "react-bootstrap/Card";
+import {Button} from 'react-bootstrap'
+import './prod.css'
+
 
 export const Product = (props) => {
   const { id, productName, price, productImage } = props.data;
@@ -8,17 +12,25 @@ export const Product = (props) => {
   const cartItemCount = cartItems[id];
 
   return (
-    <div className="product">
-      <img src={productImage} />
-      <div className="description">
-        <p>
-          <b>{productName}</b>
-        </p>
-        <p> ${price}</p>
-      </div>
-      <button className="addToCartBttn" onClick={() => addToCart(id)}>
-        Add To Cart {cartItemCount > 0 && <> ({cartItemCount})</>}
-      </button>
-    </div>
-  );
+    <div  className="col-lg-3 col-md-3 col-sm-6">
+            <div className="box">
+                <div className="img-box">
+                    <img src={productImage} />
+                       
+                    </div>
+                </div>
+                <div className="text-box">
+                    <a>{productName}</a>
+                    <span>{price}</span>
+                    <div className="action">
+                    <button className="addToCartBttn" onClick={() => addToCart(id)}>
+  Add To Cart {cartItemCount > 0 && <> ({cartItemCount})</>}
+</button>                    </div>
+                </div>
+            </div>
+    
+  
+  )
 };
+
+

ccf0653519605a7f9b1d7ee5d17d7ad8bb42e9f0
Amin-chak99 <76822547+Amin-chak99@users.noreply.github.com>
2023-01-19 19:05:10 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/navbar/navbar";
 import { Shop } from "./pages/shop/shop";
